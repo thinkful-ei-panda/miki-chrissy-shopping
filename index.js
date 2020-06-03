14146f1cc37e54e6eac4e5cc8b74894a76c8fc40
mischkiey <mischkieycf@gmail.com>
2020-06-03 14:08:49 -0400

    Updated index.js

--- index.js
+++ index.js
@@ -84,8 +84,9 @@ function handleItemCheckClicked() {
 function removeId(item) {
   console.log('`removeItem` ran');
   for (let i = 0; i < STORE.length; i++) {
-    if (id === STORE[i].id) {
-      STORE.slice(i);
+    if (item == STORE[i].id) {
+      console.log(i);
+      STORE.splice(i, i+1);
     };
   };
 }
@@ -98,6 +99,7 @@ function handleDeleteItemClicked() {
     console.log('`handleDeleteItemClicked` ran');
     const id = getItemIdFromElement(event.currentTarget);
     removeId(id);
+    renderShoppingList();
   });
 }
 
